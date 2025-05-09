5f81d34d8ea5ab60154ee9882f983d524d985c30
makotokabuki <mkabukiazure@gmail.com>
2025-05-09 15:04:39 +0900

    Hello Worldプログラムを追加

---
+++ hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+簡単なHello Worldプログラム
+作成日: 2025年5月9日
+作成者: 歌舞伎
+"""
+
+def main():
+    """
+    メイン関数
+    """
+    print("Hello, World!")
+    print("This is my first GitHub project!")
+    print("今日は2025年5月9日です。")
+
+if __name__ == "__main__":
+    main()
